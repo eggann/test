906407ad0869f370e08210ae65d8213521dce8e8
eggann <59432847+eggann@users.noreply.github.com>
2023-02-23 01:57:00 +0800

    Add files via upload

---
+++ react/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import Header from './components/Header';
+import About from './components/About';
+import Experience from './components/Experience';
+import Footer from './components/Footer';
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <NavBar />
+      <Header />
+      <About />
+      <Experience />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
